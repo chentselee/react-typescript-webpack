c0b72b4af9700a823b424bd07f555f5bc8a5be3f
Chentse Lee <chentselee@gmail.com>
2022-09-23 04:59:44 +0800

    remove unused config

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,6 @@ module.exports = {
         loader: "ts-loader",
         exclude: /node_modules/,
         options: {
-          transpileOnly: true,
           getCustomTransformers: () => ({
             before: [isDevelopment && ReactRefreshTypeScript()].filter(Boolean),
           }),
