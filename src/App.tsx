d03931ab4b92398a7154bcfb87317892dd5b5679
Chentse Lee <chentselee@gmail.com>
2025-11-26 15:24:35 +0800

    add togggle counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useState } from "react";
 import "./App.css";
 
 const Counter = lazy(() => import("./components/Counter"));
 
 const App = () => {
+  const [loaded, setLoaded] = useState(false)
   return (
-    <Suspense>
-      <div className="container">
-        <div>it works</div>
-        <Counter />
-      </div>
-    </Suspense>
+    <div className="container">
+      <div>it works</div>
+      <button onClick={() => setLoaded(prev => !prev)}>toggle counter</button>
+      <Suspense fallback={<div>Loading...</div>}>
+        {loaded && <Counter />}
+      </Suspense>
+    </div>
   );
 };
 
